9363e2222faca982b55b431726cd16663416399b
DanielDeanGithub <github-new@inboxrefresh.com>
2025-01-17 08:18:07 +0000

    Add new Route for rending dynamic path using parameters called type and id

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 const appRouter = createBrowserRouter(
   createRoutesFromElements( 
     <Route path="/" element={<Root />}>
-      <Route index element={ <HomePage  /> }/> 
-      <Route path=":type"element={ <HomePage  /> } />
+      <Route index element={ <HomePage /> }/> 
+      <Route path=":type"element={ <HomePage /> } />
+      <Route path=":type/:id"element={ <PetDetailsPage /> } />
+
     </Route>
   )
 );
