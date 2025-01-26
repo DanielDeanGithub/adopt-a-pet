62926e05b4303f43160588422f3845010897d9d7
DanielDeanGithub <github-new@inboxrefresh.com>
2025-01-26 11:50:11 +0000

    Get searchParams object from useSearchParams

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -8,6 +8,7 @@ import { useSearchParams } from 'react-router-dom';
 const SearchPage = () => {
 
   // Get searchParams object from useSearchParams
+  const [ searchParams ] = useSearchParams();
 
   const petNameToFind = 'REPLACE ME';  // Get query parameter using searchParams object
 
